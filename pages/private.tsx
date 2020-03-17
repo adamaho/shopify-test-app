ae0a194e675fd23f01723536dc2f121e98f6fdee
adamaho <aaho.public@gmail.com>
2020-03-17 16:24:33 -0400

    first commit

---
+++ pages/private.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import {
+  Button
+} from '@rtkwlf/ui';
+
+export default () => <div>This is a private route behind the shopify auth <Button>Hello</Button></div>;
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,9 @@
-import { createServer } from 'http'
-import { parse } from 'url'
+import 'isomorphic-fetch';
+
+import Koa from 'koa';
+import Router from 'koa-router';
+import session from 'koa-session';
+import shopifyAuth, {verifyRequest} from '@shopify/koa-shopify-auth';
 import next from 'next'
 
 const port = parseInt(process.env.PORT || '3000', 10)
@@ -7,24 +11,62 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const {SHOPIFY_API_KEY, SHOPIFY_SECRET} = process.env;
+
+
+
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    const { pathname, query } = parsedUrl
+  const server = new Koa();
+  const clientRouter = new Router();
+  const privateRouter = new Router();
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(port)
+  clientRouter.get('/auction', async (ctx: any) => {
+    // ctx.set('Content-Type', 'application/liquid');
+    await app.render(ctx.req, ctx.res, '/auction', ctx.query)
+    ctx.respond = false
+  })
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+  clientRouter.all('*', async (ctx: any) => {
+    await handle(ctx.req, ctx.res)
+    ctx.respond = false
+  })
+
+  privateRouter.get('/auction-data', async (ctx: any) => {
+    ctx.body = 'Return some json here using the shopify token';
+    ctx.statusCode = 200;
+  });
+
+  server.keys = [SHOPIFY_SECRET as string];
+
+  server
+    // sets up secure session data on each request
+    .use(session({ secure: true, sameSite: 'none' }, server))
+  
+    // sets up shopify auth
+    .use(
+      shopifyAuth({
+        apiKey: SHOPIFY_API_KEY as string,
+        secret: SHOPIFY_SECRET as string,
+        scopes: ['write_orders, write_products'],
+        afterAuth(ctx) {
+          const {accessToken} = ctx.session as any;
+  
+          console.log('We did it!', accessToken);
+  
+          ctx.redirect('/');
+        },
+      }),
+    )
+ 
+    // everything after this point will require authentication
+    .use(clientRouter.routes())
+    .use(verifyRequest())
+    .use(privateRouter.routes())
+
+  server.use(async (ctx: any, next: any) => {
+    ctx.res.statusCode = 200
+    await next()
+  })
+
+  server.listen(port, () => console.log(`listening at http://localhost:${port}`));
+});
